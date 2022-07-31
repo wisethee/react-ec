7160d621ad8ca843a0bea07b461a2bdf7d40bf0f
Marius Paduraru <marius.paduraru@gmail.com>
2022-07-31 20:12:02 +0100

    feat: add app routes

--- apps/ec/src/app/app.tsx
+++ apps/ec/src/app/app.tsx
@@ -1,12 +1,36 @@
+// Router
+import { Route, Routes } from 'react-router-dom';
+
+// Jss -> Theme Provider | Global Styles
 import { ThemeProvider } from 'react-jss';
-import GlobalStyles from './core/styles/global.styles';
 import { lightTheme } from './core/themes/light.theme';
+import GlobalStyles from './core/styles/global.styles';
+
+// Components -> Layout
+import AppFooter from './core/layout/footer/footer.component';
+import AppMain from './core/layout/main/main.component';
+import AppNav from './core/layout/nav/nav.component';
+
+// Feat -> Views
+import AppAuth from './feat/auth/auth.component';
+import AppHome from './feat/home/home.component';
+import AppNotFound from './feat/not-found/not-found.component';
 
 const App = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <GlobalStyles />
-      <div>App works!</div>
+      <AppNav />
+
+      <Routes>
+        <Route path="/" element={<AppMain />}>
+          <Route path="/" element={<AppHome />} />
+          <Route path="/auth" element={<AppAuth />} />
+          <Route path="*" element={<AppNotFound />} />
+        </Route>
+      </Routes>
+
+      <AppFooter />
     </ThemeProvider>
   );
 };
