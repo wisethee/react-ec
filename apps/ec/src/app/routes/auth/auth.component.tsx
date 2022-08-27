e8e2f8e3a43791c2fc9f4a622f33e64589fc08d2
marius <marius.paduraru@gmail.com>
2022-08-27 22:17:38 +0100

    feat: add sign-in and sign-up components to auth component

--- apps/ec/src/app/routes/auth/auth.component.tsx
+++ apps/ec/src/app/routes/auth/auth.component.tsx
@@ -1,5 +1,16 @@
+// Components
+import AppSignIn from '../../core/components/sign-in/sign-in.component';
+import AppSignUp from '../../core/components/sign-up/sign-up.component';
+import AppContainer from '../../core/layout/container/container.component';
+
+// @Component
 const AppAuth = () => {
-  return <div>AppAuth works!</div>;
+  return (
+    <AppContainer fluid={false}>
+      <AppSignIn />
+      <AppSignUp />
+    </AppContainer>
+  );
 };
 
 export default AppAuth;
