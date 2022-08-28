e46271806111933307e1309016412eac0867322a
marius <marius.paduraru@gmail.com>
2022-08-28 11:26:19 +0100

    feat: add firebase utils and brought in firebase libraries

--- apps/ec/src/app/core/components/sign-in/sign-in.component.tsx
+++ apps/ec/src/app/core/components/sign-in/sign-in.component.tsx
@@ -1,5 +1,19 @@
+import {
+  createUserDocuemnt,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
+
 const AppSignIn = () => {
-  return <div>AppSignIn works!</div>;
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocuemnt(user);
+  };
+
+  return (
+    <div>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+    </div>
+  );
 };
 
 export default AppSignIn;
--- apps/ec/src/app/core/utils/firebase/firebase.utils.ts
+++ apps/ec/src/app/core/utils/firebase/firebase.utils.ts
@@ -1,4 +1,11 @@
 import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  User,
+} from 'firebase/auth';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,4 +18,34 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Providers
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+
+// Auth
+const auth = getAuth(firebaseApp);
+
+// DB
+const db = getFirestore(firebaseApp);
+
+// Sign in user with google popup window
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+// Create user document from auth
+export const createUserDocuemnt = async (userAuth: User) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
